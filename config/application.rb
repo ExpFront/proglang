5493f7bb4c85c9e9b0e81c0821b8b55767dfd0c0
Andrew Abarykov <expfront@gmail.com>
2017-05-15 23:49:14 +0300

    chore(proglang): add config fix

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Shop
   class Application < Rails::Application
 
     config.i18n.default_locale = 'ru'
-
+    config.assets.initialize_on_precompile = false
     config.to_prepare do
       # Load application's model / class decorators
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
